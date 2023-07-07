c4e7562d252b3af1bdef0cb9e44cdbaa4ecf59a9
Ayush Pratap Singh <dabangayush369@gmail.com>
2023-07-07 23:04:44 +0530

    mail fix

--- mail/contact.php
+++ mail/contact.php
@@ -12,8 +12,9 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = "dabangayush369@gmail.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email";
-$header .= "Reply-To: $email";	
+$headers = "From: ".$name." <".$email.">\r\n"; $headers = "Reply-To: ".$email."\r\n"; 
+$headers = "Content-type: text/html; charset=iso-8859-1\r\n";
+'X-Mailer: PHP/' . phpversion();
 
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
